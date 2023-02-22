d3720a3bf2d806f2180e42a8eccc1ffce9d75ea6
Victor Beserra <49791510+victor-beserra@users.noreply.github.com>
2023-02-22 20:21:30 -0300

    Commit Inicial
    Inicio do projeto em react - Hello World

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Boa Noite Professor !!</h1>
+      <h2>Hello World!</h2>
+      <h3>Olá Mundo!</h3>
+    </div>
+  );
+}
+
+export default App;
